cbaf23cdce02055cd9babdb200bc878cdb650d83
Yi Sun <yi-sun@users.noreply.github.com>
2024-05-08 13:31:37 -0400

    feat: add initial version of WorldcoinAggregationV2

--- src/WorldcoinAggregationV2.sol
+++ src/WorldcoinAggregationV2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import { AxiomV2Client } from "@axiom-crypto/v2-periphery/client/AxiomV2Client.sol";
 
-contract WorldcoinAggregation is AxiomV2Client {
+contract WorldcoinAggregationV2 is AxiomV2Client {
     /// @dev The unique identifier of the circuit accepted by this contract.
     bytes32 immutable QUERY_SCHEMA;
 
@@ -13,11 +13,14 @@ contract WorldcoinAggregation is AxiomV2Client {
     /// @dev The verification key hash of the Groth16 circuit.
     bytes32 immutable VKEY_HASH;
 
-    /// @dev The maximum number of claims that can be made in a single call.
-    uint256 immutable MAX_NUM_CLAIMS;
+    /// @dev The base-2 logarithm of the maximum number of claims that can be made in a single call.
+    uint256 immutable LOG_MAX_NUM_CLAIMS;
 
     /// @dev Whether a nullifier hash has been used already. Used to prevent double-signaling
     mapping(uint256 => bool) public nullifierHashes;
+
+    /// @dev Valid Merkle roots of claims, indexed by grantId and root
+    mapping(uint256 => mapping(uint256 => mapping(bytes32 => bool))) public validClaimsRoots;
 
     /// @notice Emitted when a grant is successfully claimed
     /// @param receiver The address that received the tokens
@@ -28,49 +31,64 @@ contract WorldcoinAggregation is AxiomV2Client {
     /// @param  callbackSourceChainId The ID of the chain the query reads from.
     /// @param  querySchema The schema of the query.
     /// @param  vkeyHash The verification key hash of the Groth16 circuit.
-    /// @param  maxNumClaims The number of claims that can be made in a single call.
+    /// @param  logMaxNumClaims The number of claims that can be made in a single call.
     constructor(
         address axiomV2QueryAddress,
         uint64 callbackSourceChainId,
         bytes32 querySchema,
         bytes32 vkeyHash,
-        uint256 maxNumClaims
+        uint256 logMaxNumClaims
     ) AxiomV2Client(axiomV2QueryAddress) {
         QUERY_SCHEMA = querySchema;
         SOURCE_CHAIN_ID = callbackSourceChainId;
         VKEY_HASH = vkeyHash;
-        MAX_NUM_CLAIMS = maxNumClaims;
+        LOG_MAX_NUM_CLAIMS = logMaxNumClaims;
     }
 
-    /// @notice Claim multiple airdrops
-    /// @param numClaims The number of claims to make
+    /// @notice Claim a grant
     /// @param grantId The grant ID to claim
-    /// @param receivers The addresses that will receive the tokens (this is also the signal of the ZKP)
     /// @param root The root of the Merkle tree (signup-sequencer or world-id-contracts provides this)
-    /// @param claimedNullifierHashes The nullifiers for the proofs, preventing double signaling
-    function _batchClaim(
-        uint256 numClaims,
+    /// @param receiver The address that will receive the tokens (this is also the signal of the ZKP)
+    /// @param nullifierHash The nullifier for the proof, preventing double signaling
+    /// @param merkleProof The Merkle proof of the claim
+    function claim(
         uint256 grantId,
-        address[] memory receivers,
         uint256 root,
-        uint256[] memory claimedNullifierHashes
-    ) internal {
+        address receiver,
+        uint256 nullifierHash,
+        bytes32[] calldata merkleProof
+    ) external {
         _requireValidRoot(root);
-
         _requireValidGrant(grantId);
 
-        require(receivers.length == numClaims, "Invalid number of receivers");
-        require(claimedNullifierHashes.length == numClaims, "Invalid number of nullifiers");
+        require(!nullifierHashes[nullifierHash], "Nullifier hash already used");
+        require(receiver != address(0), "Invalid receiver");
+        require(merkleProof.length == LOG_MAX_NUM_CLAIMS, "Invalid Merkle proof length");
 
-        for (uint256 i = 0; i < numClaims; i++) {
-            if (!nullifierHashes[claimedNullifierHashes[i]] && receivers[i] != address(0)) {
-                nullifierHashes[claimedNullifierHashes[i]] = true;
-
-                // TODO: actually transfer the grant funds
-
-                emit GrantClaimed(grantId, receivers[i]);
+        // TODO: gas optimize by doing Keccak in assembly
+        bytes32 runningHash = keccak256(abi.encodePacked(receiver, nullifierHash));
+        for (uint256 i = 0; i < merkleProof.length; i++) {
+            if (runningHash < merkleProof[i]) {
+                runningHash = keccak256(abi.encodePacked(runningHash, merkleProof[i]));
+            } else {
+                runningHash = keccak256(abi.encodePacked(merkleProof[i], runningHash));
             }
         }
+        require(validClaimsRoots[grantId][root][runningHash], "Invalid Merkle proof");
+
+        nullifierHashes[nullifierHash] = true;
+
+        // TODO: actually transfer the grant funds
+
+        emit GrantClaimed(grantId, receiver);
+    }
+
+    /// @notice Record claimsRoot for multiple airdrop claims
+    /// @param grantId The grant ID to claim
+    /// @param root The root of the Merkle tree (signup-sequencer or world-id-contracts provides this)
+    /// @param claimsRoot The root of the Merkle tree of the claims
+    function _recordClaimsRoot(uint256 grantId, uint256 root, bytes32 claimsRoot) internal {
+        validClaimsRoots[grantId][root][claimsRoot] = true;
     }
 
     /// @notice Reverts if the root is not valid
@@ -110,32 +128,20 @@ contract WorldcoinAggregation is AxiomV2Client {
         bytes32[] calldata axiomResults,
         bytes calldata // extraData
     ) internal override {
-        require(axiomResults.length == 4 + 2 * MAX_NUM_CLAIMS, "Invalid number of results");
+        require(axiomResults.length == 4, "Invalid number of results");
         // We expect the results returned from the Axiom query to be:
         // axiomResults[0]: vkeyHash
         // axiomResults[1]: grantId
         // axiomResults[2]: root
-        // axiomResults[3]: numClaims
-        // axiomResults[idx] for idx in [4, 4 + numClaims): receivers
-        // axiomResults[idx] for idx in [4 + MAX_NUM_CLAIMS, 4 + MAX_NUM_CLAIMS + numClaims): claimedNullifierHashes
+        // axiomResults[3]: claimsRoot
 
         bytes32 vkeyHash = axiomResults[0];
         uint256 grantId = uint256(axiomResults[1]);
         uint256 root = uint256(axiomResults[2]);
-        uint256 numClaims = uint256(axiomResults[3]);
+        bytes32 claimsRoot = axiomResults[3];
 
         require(vkeyHash == VKEY_HASH, "Invalid vkey hash");
-        require(numClaims <= MAX_NUM_CLAIMS, "Too many claims");
 
-        address[] memory receivers = new address[](numClaims);
-        uint256[] memory claimedNullifierHashes = new uint256[](numClaims);
-        for (uint256 idx; idx < numClaims; idx++) {
-            receivers[idx] = address(uint160(uint256(axiomResults[4 + idx])));
-        }
-        for (uint256 idx; idx < numClaims; idx++) {
-            claimedNullifierHashes[idx] = uint256(axiomResults[4 + MAX_NUM_CLAIMS + idx]);
-        }
-
-        _batchClaim(numClaims, grantId, receivers, root, claimedNullifierHashes);
+        _recordClaimsRoot(grantId, root, claimsRoot);
     }
 }
