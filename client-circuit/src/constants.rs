ddc6a6336bb5c485da45858f39045d2c8328696b
PangZhi <1611805+PangZhi@users.noreply.github.com>
2024-05-20 08:29:37 -0700

    chore: update configs

--- client-circuit/src/constants.rs
+++ client-circuit/src/constants.rs
@@ -1,4 +1,4 @@
-pub const MAX_PROOFS: usize = 2;
+pub const MAX_PROOFS: usize = 16;
 pub const MAX_GROTH16_PI: usize = 4;
 pub const NUM_FE_VKEY: usize = 25;
 pub const NUM_FE_GROTH16_INPUT: usize = 38;
