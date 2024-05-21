e1dc6786020e099497a309e18812b40c053352f3
Yi Sun <yi-sun@users.noreply.github.com>
2024-05-21 12:55:06 -0400

    feat: contract for client circuit V1 (#3)
    * feat: allow a variable number of claims
    
    * feat: add vkeyHash

--- src/WorldcoinAggregation.sol
+++ src/WorldcoinAggregation.sol
@@ -10,8 +10,11 @@ contract WorldcoinAggregation is AxiomV2Client {
     /// @dev The chain ID of the chain whose data the callback is expected to be called from.
     uint64 immutable SOURCE_CHAIN_ID;
 
-    /// @dev The number of claims that can be made in a single call.
-    uint256 immutable NUM_CLAIMS;
+    /// @dev The verification key hash of the Groth16 circuit.
+    bytes32 immutable VKEY_HASH;
+
+    /// @dev The maximum number of claims that can be made in a single call.
+    uint256 immutable MAX_NUM_CLAIMS;
 
     /// @dev Whether a nullifier hash has been used already. Used to prevent double-signaling
     mapping(uint256 => bool) public nullifierHashes;
@@ -24,21 +27,29 @@ contract WorldcoinAggregation is AxiomV2Client {
     /// @param  axiomV2QueryAddress The address of the AxiomV2Query contract.
     /// @param  callbackSourceChainId The ID of the chain the query reads from.
     /// @param  querySchema The schema of the query.
-    /// @param  numClaims The number of claims that can be made in a single call.
-    constructor(address axiomV2QueryAddress, uint64 callbackSourceChainId, bytes32 querySchema, uint256 numClaims)
-        AxiomV2Client(axiomV2QueryAddress)
-    {
+    /// @param  vkeyHash The verification key hash of the Groth16 circuit.
+    /// @param  maxNumClaims The number of claims that can be made in a single call.
+    constructor(
+        address axiomV2QueryAddress,
+        uint64 callbackSourceChainId,
+        bytes32 querySchema,
+        bytes32 vkeyHash,
+        uint256 maxNumClaims
+    ) AxiomV2Client(axiomV2QueryAddress) {
         QUERY_SCHEMA = querySchema;
         SOURCE_CHAIN_ID = callbackSourceChainId;
-        NUM_CLAIMS = numClaims;
+        VKEY_HASH = vkeyHash;
+        MAX_NUM_CLAIMS = maxNumClaims;
     }
 
     /// @notice Claim multiple airdrops
+    /// @param numClaims The number of claims to make
     /// @param grantId The grant ID to claim
     /// @param receivers The addresses that will receive the tokens (this is also the signal of the ZKP)
     /// @param root The root of the Merkle tree (signup-sequencer or world-id-contracts provides this)
     /// @param claimedNullifierHashes The nullifiers for the proofs, preventing double signaling
     function _batchClaim(
+        uint256 numClaims,
         uint256 grantId,
         address[] memory receivers,
         uint256 root,
@@ -48,7 +59,10 @@ contract WorldcoinAggregation is AxiomV2Client {
 
         _requireValidGrant(grantId);
 
-        for (uint256 i = 0; i < receivers.length; i++) {
+        require(receivers.length == numClaims, "Invalid number of receivers");
+        require(claimedNullifierHashes.length == numClaims, "Invalid number of nullifiers");
+
+        for (uint256 i = 0; i < numClaims; i++) {
             if (!nullifierHashes[claimedNullifierHashes[i]] && receivers[i] != address(0)) {
                 nullifierHashes[claimedNullifierHashes[i]] = true;
                 emit GrantClaimed(grantId, receivers[i]);
@@ -93,19 +107,32 @@ contract WorldcoinAggregation is AxiomV2Client {
         bytes32[] calldata axiomResults,
         bytes calldata // extraData
     ) internal override {
-        require(axiomResults.length == 2 + NUM_CLAIMS * 2, "Invalid number of results");
+        require(axiomResults.length == 4 + 2 * MAX_NUM_CLAIMS, "Invalid number of results");
+        // We expect the results returned from the Axiom query to be:
+        // axiomResults[0]: vkeyHash
+        // axiomResults[1]: grantId
+        // axiomResults[2]: root
+        // axiomResults[3]: numClaims
+        // axiomResults[idx] for idx in [4, 4 + numClaims): receivers
+        // axiomResults[idx] for idx in [4 + MAX_NUM_CLAIMS, 4 + MAX_NUM_CLAIMS + numClaims): claimedNullifierHashes
 
-        uint256 grantId = uint256(axiomResults[0]);
-        uint256 root = uint256(axiomResults[1]);
-        address[] memory receivers = new address[](NUM_CLAIMS);
-        uint256[] memory claimedNullifierHashes = new uint256[](NUM_CLAIMS);
-        for (uint256 idx; idx < NUM_CLAIMS; idx++) {
-            receivers[idx] = address(uint160(uint256(axiomResults[2 + idx])));
+        bytes32 vkeyHash = axiomResults[0];
+        uint256 grantId = uint256(axiomResults[1]);
+        uint256 root = uint256(axiomResults[2]);
+        uint256 numClaims = uint256(axiomResults[3]);
+
+        require(vkeyHash == VKEY_HASH, "Invalid vkey hash");
+        require(numClaims <= MAX_NUM_CLAIMS, "Too many claims");
+
+        address[] memory receivers = new address[](numClaims);
+        uint256[] memory claimedNullifierHashes = new uint256[](numClaims);
+        for (uint256 idx; idx < numClaims; idx++) {
+            receivers[idx] = address(uint160(uint256(axiomResults[4 + idx])));
         }
-        for (uint256 idx; idx < NUM_CLAIMS; idx++) {
-            claimedNullifierHashes[idx] = uint256(axiomResults[2 + NUM_CLAIMS + idx]);
+        for (uint256 idx; idx < numClaims; idx++) {
+            claimedNullifierHashes[idx] = uint256(axiomResults[4 + MAX_NUM_CLAIMS + idx]);
         }
 
-        _batchClaim(grantId, receivers, root, claimedNullifierHashes);
+        _batchClaim(numClaims, grantId, receivers, root, claimedNullifierHashes);
     }
 }
--- test/WorldcoinAggregation.t.sol
+++ test/WorldcoinAggregation.t.sol
@@ -9,8 +9,10 @@ contract WorldcoinAggregationTest is AxiomTest {
     using Axiom for Query;
 
     struct AxiomInput {
+        bytes32 vkeyHash;
         uint256 grantId;
         uint256 root;
+        uint256 numClaims;
         address[] receivers;
         uint256[] claimedNullifierHashes;
     }
@@ -18,7 +20,8 @@ contract WorldcoinAggregationTest is AxiomTest {
     WorldcoinAggregation public aggregation;
     AxiomInput public input;
     bytes32 public querySchema;
-    uint256 public numClaims;
+    bytes32 public vkeyHash;
+    uint256 public maxNumClaims;
 
     function setUp() public {
         _createSelectForkAndSetupAxiom("provider");
@@ -30,14 +33,18 @@ contract WorldcoinAggregationTest is AxiomTest {
         claimedNullifierHashes[0] = 0x4b7790813c37c910b41236334ce9b1841d430e3b4874e89778e1afd0fd3a7b6;
 
         input = AxiomInput({
+            vkeyHash: bytes32(0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef),
             grantId: 31,
             root: 0x1d0372864732dfcd91c18414fd4126e1e38293be237aad4315a026bf23d02717,
+            numClaims: 1,
             receivers: receivers,
             claimedNullifierHashes: claimedNullifierHashes
         });
         querySchema = axiomVm.readCircuit("app/axiom/worldcoin.circuit.ts");
-        numClaims = 1;
-        aggregation = new WorldcoinAggregation(axiomV2QueryAddress, uint64(block.chainid), querySchema, numClaims);
+        vkeyHash = bytes32(0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef);
+        maxNumClaims = 1;
+        aggregation =
+            new WorldcoinAggregation(axiomV2QueryAddress, uint64(block.chainid), querySchema, vkeyHash, maxNumClaims);
     }
 
     /// @dev Simple demonstration of testing an Axiom client contract using Axiom cheatcodes
