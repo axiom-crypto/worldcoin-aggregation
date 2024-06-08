f97d76aa0608758a18be6192c1a8d68065559e2a
Hrik Bhowal <23041116+HrikB@users.noreply.github.com>
2024-06-07 19:09:51 -0700

    test: e2e test using rust sdk support from axiom-std

--- client-circuit/src/bin/run_v2_circuit.rs
+++ client-circuit/src/bin/run_v2_circuit.rs
@@ -4,7 +4,6 @@ use client_circuit::circuit_v2::WorldcoinV2Circuit;
 use client_circuit::types::WorldcoinNativeInput;
 
 pub fn main() {
-    println!("Running v2 circuit");
     env_logger::init();
     let cli = AxiomCircuitRunnerOptions::parse();
     run_cli_on_scaffold::<WorldcoinV2Circuit, WorldcoinNativeInput>(cli);
