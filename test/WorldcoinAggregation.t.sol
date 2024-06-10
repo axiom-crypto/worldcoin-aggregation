4d7be8d9737157d897006f99bf885f6e32cfc078
HrikB <23041116+HrikB@users.noreply.github.com>
2024-06-10 08:12:05 -0700

    End-to-end test with Rust sdk (#9)
    * chore: remove axiom-std dep
    
    * forge install: axiom-std
    
    dbbb43aec8605ea71b98c9ad8088adc9751d5b80
    
    * test: e2e testing of the rust zk circuits
    
    * chore: update CI
    
    * chore: update CI
    
    * fix: bump logMaxNumClaims on V2 test to 4
    
    ---------
    
    Co-authored-by: Yi Sun <yi-sun@users.noreply.github.com>

--- test/WorldcoinAggregation.t.sol
+++ test/WorldcoinAggregation.t.sol
@@ -2,23 +2,16 @@
 pragma solidity ^0.8.19;
 
 import "@axiom-crypto/axiom-std/AxiomTest.sol";
-
 import { WorldcoinAggregation } from "../src/WorldcoinAggregation.sol";
 
 contract WorldcoinAggregationTest is AxiomTest {
     using Axiom for Query;
 
     struct AxiomInput {
-        bytes32 vkeyHash;
-        uint256 grantId;
-        uint256 root;
-        uint256 numClaims;
-        address[] receivers;
-        uint256[] claimedNullifierHashes;
+        bytes32 e;
     }
 
     WorldcoinAggregation public aggregation;
-    AxiomInput public input;
     bytes32 public querySchema;
     bytes32 public vkeyHash;
     uint256 public maxNumClaims;
@@ -26,23 +19,14 @@ contract WorldcoinAggregationTest is AxiomTest {
     function setUp() public {
         _createSelectForkAndSetupAxiom("provider");
 
-        address[] memory receivers = new address[](1);
-        receivers[0] = address(0x787878);
-
-        uint256[] memory claimedNullifierHashes = new uint256[](1);
-        claimedNullifierHashes[0] = 0x4b7790813c37c910b41236334ce9b1841d430e3b4874e89778e1afd0fd3a7b6;
-
-        input = AxiomInput({
-            vkeyHash: bytes32(0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef),
-            grantId: 31,
-            root: 0x1d0372864732dfcd91c18414fd4126e1e38293be237aad4315a026bf23d02717,
-            numClaims: 1,
-            receivers: receivers,
-            claimedNullifierHashes: claimedNullifierHashes
-        });
-        querySchema = axiomVm.readCircuit("app/axiom/worldcoin.circuit.ts");
-        vkeyHash = bytes32(0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef);
-        maxNumClaims = 1;
+        querySchema = axiomVm.readRustCircuit(
+            "client-circuit/Cargo.toml",
+            "client-circuit/data/worldcoin_input.json",
+            "client-circuit/data",
+            "run_v1_circuit"
+        );
+        vkeyHash = bytes32(0x46e72119ce99272ddff09e0780b472fdc612ca799c245eea223b27e57a5f9cec);
+        maxNumClaims = 16;
         aggregation =
             new WorldcoinAggregation(axiomV2QueryAddress, uint64(block.chainid), querySchema, vkeyHash, maxNumClaims);
     }
@@ -50,11 +34,10 @@ contract WorldcoinAggregationTest is AxiomTest {
     /// @dev Simple demonstration of testing an Axiom client contract using Axiom cheatcodes
     function test_simple_example() public {
         // create a query into Axiom with default parameters
-        Query memory q = query(querySchema, abi.encode(input), address(aggregation));
+        Query memory q = query(querySchema, "", address(aggregation));
 
         // send the query to Axiom
         q.send();
-
         // prank fulfillment of the query, returning the Axiom results
         bytes32[] memory results = q.prankFulfill();
     }
--- test/WorldcoinAggregationV2.t.sol
+++ test/WorldcoinAggregationV2.t.sol
@@ -2,21 +2,16 @@
 pragma solidity ^0.8.19;
 
 import "@axiom-crypto/axiom-std/AxiomTest.sol";
-
 import { WorldcoinAggregationV2 } from "../src/WorldcoinAggregationV2.sol";
 
 contract WorldcoinAggregationV2Test is AxiomTest {
     using Axiom for Query;
 
     struct AxiomInput {
-        bytes32 vkeyHash;
-        uint256 grantId;
-        uint256 root;
-        bytes32 claimsRoot;
+        bytes32 e;
     }
 
     WorldcoinAggregationV2 public aggregation;
-    AxiomInput public input;
     bytes32 public querySchema;
     bytes32 public vkeyHash;
     uint256 public logMaxNumClaims;
@@ -24,29 +19,28 @@ contract WorldcoinAggregationV2Test is AxiomTest {
     function setUp() public {
         _createSelectForkAndSetupAxiom("provider");
 
-        input = AxiomInput({
-            vkeyHash: bytes32(0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef),
-            grantId: 31,
-            root: 0x1d0372864732dfcd91c18414fd4126e1e38293be237aad4315a026bf23d02717,
-            claimsRoot: bytes32(0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef)
-        });
-        querySchema = axiomVm.readCircuit("app/axiom/worldcoinV2.circuit.ts");
-        vkeyHash = bytes32(0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef);
-        logMaxNumClaims = 1;
+        querySchema = axiomVm.readRustCircuit(
+            "client-circuit/Cargo.toml",
+            "client-circuit/data/worldcoin_input.json",
+            "client-circuit/data",
+            "run_v2_circuit"
+        );
+        vkeyHash = bytes32(0x46e72119ce99272ddff09e0780b472fdc612ca799c245eea223b27e57a5f9cec);
+        logMaxNumClaims = 4;
         aggregation = new WorldcoinAggregationV2(
             axiomV2QueryAddress, uint64(block.chainid), querySchema, vkeyHash, logMaxNumClaims
         );
     }
-
     /// @dev Simple demonstration of testing an Axiom client contract using Axiom cheatcodes
+
     function test_simple_example() public {
         // create a query into Axiom with default parameters
-        Query memory q = query(querySchema, abi.encode(input), address(aggregation));
+        Query memory q = query(querySchema, "", address(aggregation));
 
         // send the query to Axiom
         q.send();
 
         // prank fulfillment of the query, returning the Axiom results
-        bytes32[] memory results = q.prankFulfill();
+        q.prankFulfill();
     }
 }
