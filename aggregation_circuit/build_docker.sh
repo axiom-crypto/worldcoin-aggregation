340501b59aa06ce49f99f49afb945ecee317fd78
PangZhi <1611805+PangZhi@users.noreply.github.com>
2024-08-16 15:15:07 -0700

    chore: update build script

--- aggregation_circuit/build_docker.sh
+++ aggregation_circuit/build_docker.sh
@@ -1,12 +1,15 @@
 #!bin/bash
 
+# usage: bash build_docker.sh <version - v1 or v2>
 REPO_ROOT=$(git rev-parse --show-toplevel)
 WORKDIR=$REPO_ROOT/aggregation_circuit
 export CARGO_HOME=$WORKDIR/.cargo
 cd $WORKDIR
 
+VERSION=${1:-v1}
+
 cargo clean
 cargo update
 cargo fetch
 
-docker build -t server .
+docker build --build-arg VERSION=${VERSION} -t server .
