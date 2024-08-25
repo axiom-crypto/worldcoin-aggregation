863b053ea8e007939c0444daa37093b344ba805a
PangZhi <1611805+PangZhi@users.noreply.github.com>
2024-08-25 14:15:13 -0700

    chore: delay keccak to root

--- standalone_circuit/src/circuits/v1/root.rs
+++ standalone_circuit/src/circuits/v1/root.rs
@@ -6,9 +6,15 @@
 //! The difference between Intermediate and Root aggregation circuits is that they expose different public outputs. Root aggregation
 //! exposes the hash of the output [vk_hash_hi, vk_hash_lo, root, num_proofs, ...grant_ids, ...receivers, ...nullifier_hashes]
 
+use std::ptr::null;
+
 use anyhow::{bail, Result};
 use axiom_eth::{
-    halo2_base::AssignedValue,
+    halo2_base::{
+        gates::{flex_gate::threads::parallelize_core, GateInstructions, RangeInstructions},
+        safe_types::SafeBool,
+        AssignedValue,
+    },
     halo2_proofs::poly::{commitment::ParamsProver, kzg::commitment::ParamsKZG},
     halo2curves::bn256::{Bn256, Fr},
     mpt::MPTChip,
@@ -19,14 +25,18 @@ use axiom_eth::{
     },
     utils::{
         build_utils::aggregation::CircuitMetadata,
+        circuit_utils::unsafe_lt_mask,
         eth_circuit::EthCircuitInstructions,
+        hilo::HiLo,
         keccak::decorator::RlcKeccakCircuitImpl,
         snark_verifier::{get_accumulator_indices, NUM_FE_ACCUMULATOR},
         uint_to_bytes_be,
     },
 };
 
-use itertools::Itertools;
+use itertools::{izip, Itertools};
+
+use crate::utils::compute_keccak_for_branch_nodes;
 
 use super::intermediate::WorldcoinIntermediateAggregationInput;
 
@@ -120,36 +130,100 @@ impl EthCircuitInstructions<Fr> for WorldcoinRootAggregationInput {
             max_depth,
         );
 
+        let gate = range.gate();
+
+        let selector: Vec<SafeBool<Fr>> = unsafe_lt_mask(ctx, gate, num_proofs, 1 << max_depth);
+
+        let mut public_inputs: Vec<[AssignedValue<Fr>; 3]> = Vec::new();
+
+        // pi[0] root
+        // pi[1] nullifier_hash
+        // pi[2] signal_hash from receiver
+        // pi[3] grant_id
+        let root = new_instances[4];
+
+        let max_proofs: usize = 1 << max_depth;
+        for i in 0..max_proofs {
+            let grant_id = new_instances[5 + i];
+            let receiver = new_instances[5 + max_proofs + i];
+            let nullifier_hash = new_instances[5 + 2 * max_proofs + i];
+
+            public_inputs.push([nullifier_hash, receiver, grant_id]);
+        }
+
+        let inputs: Vec<([AssignedValue<Fr>; 3], SafeBool<Fr>)> =
+            izip!(public_inputs, selector).collect();
+
+        let mut leaves = parallelize_core(builder.base.pool(0), inputs, |ctx, input| {
+            let (public_inputs, mask) = input;
+
+            let [nullifier_hash, receiver, grant_id] = public_inputs;
+
+            // use mask to calculate the correct leaf
+            // Leaves: keccak256(abi.encodePacked(grant_ids_i, receivers_i, nullifierHashes_i))
+            // Leaves with indices greater than num_proofs - 1 are given by keccak256(abi.encodePacked(uint256(0), address(0), bytes32(0)))
+            let mut bytes = Vec::new();
+            let masked_grant_id = gate.mul(ctx, grant_id, mask);
+            let masked_receiver = gate.mul(ctx, receiver, mask);
+            let masked_nullifier_hash = gate.mul(ctx, nullifier_hash, mask);
+            bytes.extend(uint_to_bytes_be(ctx, range, &masked_grant_id, 32));
+            bytes.extend(uint_to_bytes_be(ctx, range, &masked_receiver, 20));
+            bytes.extend(uint_to_bytes_be(ctx, range, &masked_nullifier_hash, 32));
+            let bytes = bytes.iter().map(|sb| *sb.as_ref()).collect();
+            let keccak_hash = keccak.keccak_fixed_len(ctx, bytes);
+
+            HiLo::from_hi_lo([keccak_hash.output_hi, keccak_hash.output_lo])
+        });
+
+        assert!(leaves.len().is_power_of_two());
+
+        while (leaves.len() > 1) {
+            let inputs = leaves.chunks(2).collect();
+
+            leaves = parallelize_core(builder.base.pool(0), inputs, |ctx, input| {
+                let left = input[0];
+                let right = input[1];
+                compute_keccak_for_branch_nodes(ctx, range, keccak, &left, &right)
+            });
+        }
+        let claim_root = leaves[0];
+
+        //let ctx: &mut axiom_eth::halo2_base::Context<Fr> = builder.base.main(0);
+
         // output:  [vk_hash_hi, vk_hash_lo, root, num_proofs, ...grant_ids, ...receivers, ...nullifier_hashes]
-        let mut output: Vec<AssignedValue<Fr>> = Vec::new();
-        output.extend(new_instances[2..5].to_vec());
-        output.extend([num_proofs].to_vec());
-        output.extend(new_instances[5..5 + 3 * (1 << max_depth)].to_vec());
+        // let mut output: Vec<AssignedValue<Fr>> = Vec::new();
+        // output.extend(new_instances[2..5].to_vec());
+        // output.extend([num_proofs].to_vec());
+        // output.extend(claim_root.hi_lo());
+        //output.extend(new_instances[5..5 + 3 * (1 << max_depth)].to_vec());
 
-        // generate keccak hash for the outputs
-        let output_bytes: Vec<AssignedValue<Fr>> = output
-            .iter()
-            .map(|b| {
-                let bytes: Vec<AssignedValue<Fr>> = uint_to_bytes_be(ctx, range, b, 32)
-                    .iter()
-                    .map(|sb| *sb.as_ref())
-                    .collect();
-                bytes
-            })
-            .flatten()
-            .collect();
+        // // generate keccak hash for the outputs
+        // let output_bytes: Vec<AssignedValue<Fr>> = output
+        //     .iter()
+        //     .map(|b| {
+        //         let bytes: Vec<AssignedValue<Fr>> = uint_to_bytes_be(ctx, range, b, 32)
+        //             .iter()
+        //             .map(|sb| *sb.as_ref())
+        //             .collect();
+        //         bytes
+        //     })
+        //     .flatten()
+        //     .collect();
 
-        assert_eq!(output_bytes.len(), 32 * (4 + 3 * (1 << max_depth)));
+        // assert_eq!(output_bytes.len(), 32 * (4 + 3 * (1 << max_depth)));
 
-        let output_hash = keccak.keccak_fixed_len(ctx, output_bytes);
+        // let output_hash = keccak.keccak_fixed_len(ctx, output_bytes);
 
         let assigned_instances: &mut Vec<AssignedValue<Fr>> =
             &mut builder.base.assigned_instances[0];
 
         assigned_instances.extend(accumulator);
-        assigned_instances.extend([output_hash.output_hi, output_hash.output_lo]);
+        assigned_instances.extend(new_instances[2..5].to_vec());
+        assigned_instances.extend([num_proofs].to_vec());
+        assigned_instances.extend(claim_root.hi_lo());
+        //assigned_instances.extend([output_hash.output_hi, output_hash.output_lo]);
 
-        assert_eq!(assigned_instances.len(), NUM_FE_ACCUMULATOR + 2);
+        assert_eq!(assigned_instances.len(), NUM_FE_ACCUMULATOR + 6);
     }
 
     fn virtual_assign_phase1(
@@ -166,6 +240,6 @@ impl CircuitMetadata for WorldcoinRootAggregationInput {
     const HAS_ACCUMULATOR: bool = true;
 
     fn num_instance(&self) -> Vec<usize> {
-        vec![NUM_FE_ACCUMULATOR + 2]
+        vec![NUM_FE_ACCUMULATOR + 6]
     }
 }
